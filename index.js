a3fc42c372048667b40e32a0d762c4b9415b9cf3
LeoBarreto251 <leandro.barreto26@compscihigh.org>
2026-05-08 19:23:26 +0000

    Working on ejs

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ app.use((req, res, next) => {
     next();
 });
 
+const ItemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    category: { type: String },
+    image: { type: String },
+    price: {type: Number},
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model("Item", ItemSchema, "Items");
+
+
+app.get("/", async (req, res) => {
+  const items = await Item.find({}).sort({ createdAt: -1 });
+  res.render("home.ejs", {  });
+});
+
+
 
 async function startServer() {
     await mongoose.connect("mongodb+srv://SE12:CSH2026@cluster0.ytvmkmf.mongodb.net/?appName=Cluster0")
